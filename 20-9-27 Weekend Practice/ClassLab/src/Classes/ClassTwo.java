927a9aa273d115467c23dc70bd7873a76760c54b
erichlee9 <eriche.dev@gmail.com>
2020-09-27 19:42:47 -0400

    sunday evening push

--- 20-9-27 Weekend Practice/ClassLab/src/Classes/ClassTwo.java
+++ 20-9-27 Weekend Practice/ClassLab/src/Classes/ClassTwo.java
@@ -21,13 +21,13 @@ public class ClassTwo {
 		System.out.println(id1.getId());
 		System.out.println(id3);
 		id1.toString();
-		System.out.println(id1);
+		System.out.println(id1.getId());
 		
 		//if(id1.getId()) {
 		//	System.out.println("The same");
 		//}else {System.out.println("Not same");
 	}
 		//System.out.println(id1.getId());
-
+//issue here because can't invoke method .equals on int, can't invoke method toString() on int either
 }
 
---
+++ 20-9-27 Weekend Practice/MethodLab/src/MethodOverloads/Methods.java
@@ -0,0 +1,18 @@
+package MethodOverloads;
+
+
+public class Methods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public int add(int a, int b) {
+		return a+b;
+	}
+	
+	public double add(double a, double b) {
+		return a+b;
+	}
+//i'm skipping the rest of this practice because I understand it
+}
